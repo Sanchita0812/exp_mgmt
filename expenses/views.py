f15107af89b1b077b49080b1eb0b39838b0a22ac
Sanchita0812 <sanchitakiran08@gmail.com>
2026-06-02 19:21:48 +0530

    Created API view

--- expenses/views.py
+++ expenses/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Expense
+from .serializers import ExpenseSerializer
+
+class ExpenseCreateView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+
+        serializer = ExpenseSerializer(
+            data=request.data
+        )
+
+        if serializer.is_valid():
+
+            serializer.save(user=request.user)
+
+            return Response(serializer.data)
+
+        return Response(serializer.errors)
