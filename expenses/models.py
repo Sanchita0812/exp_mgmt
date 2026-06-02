b11809b2df7975d4c8edcb8705f46f81ca010e94
Sanchita0812 <sanchitakiran08@gmail.com>
2026-06-02 19:08:31 +0530

    Added db models forexpenses + indices as per requirements

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Expense(models.Model):
+
+    STATUS_CHOICES = (
+        ('PENDING', 'Pending'),
+        ('APPROVED', 'Approved'),
+        ('REJECTED', 'Rejected'),
+    )
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+
+    title = models.CharField(max_length=255)
+
+    description = models.TextField()
+
+    original_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    original_currency = models.CharField(max_length=10)
+
+    converted_amount_inr = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default='PENDING'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['user']),
+            models.Index(fields=['status']),
+            models.Index(fields=['created_at']),
+        ]
