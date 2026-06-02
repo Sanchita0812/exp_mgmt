a47b5d2c7aed28bb6751209c9d3dcce6924c4291
Sanchita0812 <sanchitakiran08@gmail.com>
2026-06-02 16:26:24 +0530

    Added config files for JWT Auth

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # Custom user model
 AUTH_USER_MODEL = 'users.User'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
