23b84c7dbe114564102b973d5b07ba9798cd70fb
Sanchita0812 <sanchitakiran08@gmail.com>
2026-06-02 16:03:17 +0530

    Initial Django expense management setup + db table

---
+++ users/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('ADMIN', 'Admin'),
+        ('MANAGER', 'Manager'),
+        ('EMPLOYEE', 'Employee'),
+    )
+
+    role = models.CharField(
+        max_length=20,
+        choices=ROLE_CHOICES
+    )
+
+    manager = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
